2bf3ad475ba175ff6c6df2a55884d2d003b3ed0e
zollesh <harry.zolles@voestalpine.com>
2026-09-01 09:03:35 +0200

    2. commit

--- src/main/java/org/pirate/sportwebserver/service/SchedulerService.java
+++ src/main/java/org/pirate/sportwebserver/service/SchedulerService.java
@@ -1,7 +1,9 @@
 package org.pirate.sportwebserver.service;
 
+import org.pirate.sportwebserver.controller.DbConnectionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,9 @@ public class SchedulerService
 	
 	private static final Logger log = LoggerFactory.getLogger(SchedulerService.class);
 
+	@Autowired
+	private DbConnectionService dbConnection;
+	
 	@Value("${testvar:default-testvar}")
 	private String testvar;
 	
@@ -29,6 +34,14 @@ public class SchedulerService
     public void everyMinute() 
     {
     	log.info("TestService - Running every minute, testvar={}", testvar);
+    	testDBConnection();
     }
+	
+	private void testDBConnection() 
+	{
+		log.info("Test DB connection every Minute");
+		dbConnection.testConnection();
+		log.info("TestService - DB connection test completed");
+	}
 	
 }
